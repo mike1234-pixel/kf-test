8356db8b8654fbd6cd151cdde42c9ea8b2568f01
Mike <miketandy@protonmail.com>
2024-02-09 17:41:28 +0000

    wrap 404 in layout component

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
 import styles from "./NotFound.module.css"
 import logo from "assets/logo.svg"
+import { Layout } from "components/Layout"
 
 export const NotFound = () => {
   return (
-    <div className={styles.root}>
-      <img src={logo} alt='krakenflex logo' />
-      <h1>404 not found</h1>
-      <Link to='/' className={styles.link}>
-        Return Home
-      </Link>
-    </div>
+    <Layout>
+      <div className={styles.root}>
+        <img src={logo} alt='krakenflex logo' />
+        <h1>404 not found</h1>
+        <Link to='/' className={styles.link}>
+          Return Home
+        </Link>
+      </div>
+    </Layout>
   )
 }
